e8822b6d2e3841a804db041b3b5fb6ea99f8dfdc
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-13 14:04:13 +0100

    added function and further tests for initializeDataLocation

--- R/initializePersistentDataLocation.R
+++ R/initializePersistentDataLocation.R
@@ -1,4 +1,18 @@
-initializePersistenDataLocation <- function(path) {
-    # structure_path <- file.path(path, "internal_structure")
-    # dir.create(structure_path, showWarnings = FALSE)
+initializeDataLocation <- function(path) {
+    if (!dir.exists(path)) {
+        stop("Can't create data folder in not existing path: ", path)
+    }
+
+    structure_path <- file.path(path, "internal_structure")
+    if (file.exists(file.path(structure_path, "Level2.rds"))) {
+        stop("S4Level2 location has already been initialized before. 'resetDataLocation()' can be used to reset.")
+    }
+
+    dir.create(structure_path, showWarnings = FALSE)
+    level2 <- Level2(structure_path)
+    saveL2Object(level2)
 }
+
+# for(plot.init.file in dir("plot_setup_files", full.names = TRUE)) {
+#     source(plot.init.file)
+# }
--- tests/testthat/test_initializePersistentDataLocation.R
+++ tests/testthat/test_initializePersistentDataLocation.R
@@ -1,14 +1,14 @@
 context("Initialization of S4Level2 Project structure")
 clean_temp <- function() {
     temp_contents <- dir(tempdir(), full.names = TRUE)
-    unlink(temp_contents)
+    unlink(temp_contents, recursive = TRUE)
 }
 
 test_that("Folders are corectly initialized in an existing directory", {
     clean_temp()
     test_dir <- tempdir()
 
-    initializePersistentDataLocation(test_dir)
+    initializeDataLocation(test_dir)
 
     created_folders <- dir(test_dir)
     expect_equal(created_folders, "internal_structure")
@@ -18,5 +18,14 @@ test_that("Error is thrown if base folder does not exist", {
     clean_temp()
     not_exisiting_path <- file.path("g:", "is", "quite", "unrealistic")
 
-    expect_error(initializePersistentDataLocation(not_exisiting_path))
+    expect_error(initializeDataLocation(not_exisiting_path))
+})
+
+test_that("Error is thrown that folder was already initialized and should be reset if needed", {
+    clean_temp()
+
+    initializeDataLocation(tempdir())
+
+    expect_error(initializeDataLocation(tempdir()),
+                 "has already been initialized before")
 })
