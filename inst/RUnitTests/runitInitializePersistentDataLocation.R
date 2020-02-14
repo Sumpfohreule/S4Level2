e89eda52921229f4af80f58343ec0fbf65865c2a
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-14 09:50:45 +0100

    Removed duplicated test from RUnit

--- inst/RUnitTests/runitInitializePersistentDataLocation.R
+++
@@ -1,8 +0,0 @@
-testFoldersInitialized <- function() {
-    test_dir <- tempdir()
-
-    initializePersistentDataLocation(test_dir)
-
-    created_folders <- dir(test_dir)
-    RUnit::checkEquals(target = c("internal_structure"))
-}
