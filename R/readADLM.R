03be88255936db2005524a758f4f0e189c7eb84a
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-14 13:44:31 +0100

    Some more namespace clarifications

--- R/readADLM.R
+++ R/readADLM.R
@@ -1,7 +1,7 @@
 ########################################################################################################################
 # TODO: Include check if first row is strings
 readADLM <- function(path) {
-    in.table <- as.data.table(read.xlsx(path))
+    in.table <- data.table::as.data.table(openxlsx::read.xlsx(path))
     in.table <- in.table[-1]
     for (col.name in names(in.table)) {
         in.table[, (col.name) := as.numeric(get(col.name))]
@@ -9,6 +9,6 @@ readADLM <- function(path) {
     in.table[, Datum := MyUtilities::as.POSIXctFixed(Datum * 60 * 60 * 24, tz = "UTC",
             origin = "1899-12-30")]
     in.table[, Datum := MyUtilities::roundPOSIXct(Datum, 60)]
-    setkey(in.table, Datum)
+    data.table::setkey(in.table, Datum)
     return(in.table)
 }
