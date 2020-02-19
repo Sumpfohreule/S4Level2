0d6b920a120a1b914f84fc23b9e9d0bd0917f405
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-19 18:05:09 +0100

    now initializing directories when needed

--- R/addCompletePlotFromXml.R
+++ R/addCompletePlotFromXml.R
@@ -5,7 +5,10 @@ addCompletePlotFromXml <- function(level2, xml_path) {
     screened_data_path <- xml2::xml_attr(xml_root, attr = "screened_data_path")
 
     level2 <- createAndAddPlot(level2, plot_name, screened_data_path)
-
+    plot_dir <- level2 %>%
+        getPlot(Level2URI(plot_name)) %>%
+        getLocalDirectory()
+    dir.create(plot_dir, showWarnings = FALSE)
     logger_nodes <- xml2::xml_children(xml_root)
 
     for (logger_node in logger_nodes) {
@@ -13,6 +16,11 @@ addCompletePlotFromXml <- function(level2, xml_path) {
         sub_plot_name <- xml2::xml_attr(logger_node, "sub_plot")
         sub_plot_uri <- Level2URI(plot_name, sub_plot_name)
         level2 <- createAndAddSubPlot(level2, sub_plot_name, sub_plot_uri)
+        sub_plot_dir <- level2 %>%
+            getSubPlot(sub_plot_uri) %>%
+            getLocalDirectory()
+        dir.create(sub_plot_dir, showWarnings = FALSE)
+
 
         # Create and add Logger
         logger_type <- xml2::xml_attr(logger_node, "type")
@@ -34,6 +42,10 @@ addCompletePlotFromXml <- function(level2, xml_path) {
                                          source_paths = source_paths,
                                          .URI = logger_uri)
         }
+        logger_dir <- level2 %>%
+            getDataStructure(logger_uri) %>%
+            getLocalDirectory()
+        dir.create(logger_dir, showWarnings = FALSE)
 
         # Add Sensor Mappings
         mapping_table <- xml_readMappings(logger_node)
