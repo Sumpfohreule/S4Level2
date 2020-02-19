75dd993774252ca158cfb925a5c70ef2c3c9751f
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-19 15:14:20 +0100

    added optional AccessDB information

--- R/xml_createLogger.R
+++ R/xml_createLogger.R
@@ -1,12 +1,34 @@
-xml_createLogger <- function(type, sub_plot, source_paths, unique_name = type) {
-    node <- xml2::xml_new_root(.value = "Logger",
-                               name = unique_name,
-                               type = type,
-                               sub_plot = sub_plot)
+xml_createLogger <- function(type,
+                             sub_plot,
+                             source_paths,
+                             unique_name = type,
+                             db_table_name = NULL,
+                             date_column = NULL) {
+    if (type == "AccessDB") {
+        if (is.null(db_table_name) || is.null(date_column)) {
+            stop("If Logger of Type AccessDB is created a db_table_name and date_column need to be provided!")
+        }
+        node <- xml2::xml_new_root(
+            .value = "Logger",
+            name = unique_name,
+            type = type,
+            sub_plot = sub_plot,
+            db_table_name = db_table_name,
+            date_column = date_column)
+    } else {
+        node <- xml2::xml_new_root(
+            .value = "Logger",
+            name = unique_name,
+            type = type,
+            sub_plot = sub_plot)
+    }
+
+    Encoding(source_paths) <- "UTF-8"
     for (single_path in source_paths) {
-        xml2::xml_add_child(node,
-                            .value = "Source_Path",
-                            path = single_path)
+        xml2::xml_add_child(
+            node,
+            .value = "Source_Path",
+            path = single_path)
     }
     return(node)
 }
