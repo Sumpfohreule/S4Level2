09aaff49b39db7c9e49b36032a0268b76960756b
Lars Isenberg <isenberg.lars@gmail.com>
2020-04-27 14:28:38 +0200

    now copying xmls to the top structure dir

--- R/initializePlotsFromXml.R
+++ R/initializePlotsFromXml.R
@@ -1,15 +1,14 @@
 initializePlotsFromXml <- function(level2, xml_parent_dir) {
     all_xml <- dir(xml_parent_dir, full.names = TRUE, pattern = "\\.xml$")
+    local_dir <- getLocalDirectory(level2)
     for (xml_path in all_xml) {
         level2 <- addCompletePlotFromXml(level2, xml_path = xml_path)
+        file.copy(from = xml_path, to = local_dir)
 
         plot_name <- stringr::str_match(xml_path, pattern = "(?<=\\/)[[:alnum:]]+(?=_setup.xml$)") %>%
             MyUtilities::toUpperByIndex(1)
         plot <- level2 %>%
             getPlot(Level2URI(plot_name))
-        local_dir <- plot %>%
-            getLocalDirectory()
-        file.copy(from = xml_path, to = local_dir)
 
         output_dir <- plot %>%
             getOutputDirectory()
