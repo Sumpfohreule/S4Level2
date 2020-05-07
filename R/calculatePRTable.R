a58db67474f037fb98350e0ee4c3c23561d991eb
Lars Isenberg <isenberg.lars@gmail.com>
2020-05-07 16:58:42 +0200

    fixed missing SUM aggregation for Casella sensor

--- R/calculatePRTable.R
+++ R/calculatePRTable.R
@@ -1,7 +1,7 @@
 ########################################################################################################################
 calculatePRTable <- function(long.l2.table) {
     # Summation of all PR columns (Niederschlag, Stammabfluss)
-    pr.sum.table <- long.l2.table[stringr::str_detect(variable, "PR_?[XYZ]?|Pluvio_mm") & !is.na(value), .(
+    pr.sum.table <- long.l2.table[stringr::str_detect(variable, "PR_?[XYZ]?|Pluvio_mm|Niederschlag\\.Casella") & !is.na(value), .(
             Datum,
             value = cumsum(value)),
         by = .(Plot, SubPlot, variable = paste0(variable, "_SUM"))]
