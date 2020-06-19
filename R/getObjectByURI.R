b7897a2b01c0f3aa93eb3fb81433a37905418202
Lars Isenberg <isenberg.lars@gmail.com>
2020-06-19 17:12:40 +0200

    exporting getObjectByURI for use by users

--- R/getObjectByURI.R
+++ R/getObjectByURI.R
@@ -1,3 +1,8 @@
+#' Returns a Level2 Object contained within and is specified by the URI
+#'
+#' @param .Object An object of type Level2, SubPlot, Plot or DataStructure
+#' @param .level2_uri An Level2URI or string that is equivalent
+#' @export
 setGeneric("getObjectByURI", def = function(.Object, level2_uri) {
     standardGeneric("getObjectByURI")
 })
