8a4e2edb75afaf067cad7e2ca8fac6612fa2353d
Lars Isenberg <isenberg.lars@gmail.com>
2020-06-19 17:22:51 +0200

    Added better assertthat errror message

--- R/is.S4Level2URI.R
+++ R/is.S4Level2URI.R
@@ -1,5 +1,10 @@
 ########################################################################################################################
 is.Level2URI <- function(x) {
-    is_object_an_uri <- inherits(x, "Level2URI")
-    return(is_object_an_uri)
+    is_level2_uri <- inherits(x, "Level2URI")
+    return(is_level2_uri)
+}
+
+#' @export
+assertthat::on_failure(is.Level2URI) <- function(call, env) {
+    paste0(deparse(call$x), " is not a Level2URI")
 }
