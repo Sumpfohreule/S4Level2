8a502af7a29fadbc317590a34b0d35ea93074608
Lars Isenberg <isenberg.lars@gmail.com>
2020-06-19 15:40:49 +0200

    added handling of multiple sheets for loadCorrectedData

--- R/2.0.0-Plot.R
+++ R/2.0.0-Plot.R
@@ -211,36 +211,47 @@ setMethod("getData", signature = "Plot", definition = function(
 })
 
 #' Load the data of corrected files from predefined folders of this plot
-#' @param sheet.name String to determine which kind of SubPlot should be loaded from each file
+#' @param sheet_names String vector to determine which kind of SubPlot should be loaded from each file
 #' @param years Numeric vector of years to load from the plot corrected files
 #' @return A data.table with all the data combined
 #' @include loadCorrectedData.R
 #' @export
-setMethod("loadCorrectedData", signature = "Plot", definition = function(.Object, sheet.name, years) {
+setMethod("loadCorrectedData", signature = "Plot", definition = function(.Object, sheet_names, years) {
     full_data <- .Object %>%
         getCorrectedAggregatePath() %>%
         dir(full.names = TRUE) %>%
         purrr::keep(~ is.null(years) || (TRUE %in% (basename(.x) %in% as.character(years)))) %>%
         purrr::map(~ {
+            path <- .x
+            sheet_names %>%
+                purrr::map(~ c(path, .x))
+            }) %>%
+        purrr::flatten() %>%
+        purrr::map(~ {
             tryCatch({
+                path <- .x[1]
+                sheet <- .x[2]
                 wide_import <- MyUtilities::getLastModifiedFile(
-                    folder = .x,
+                    folder = path,
                     pattern = "\\.xlsx$",
                     recursive = TRUE) %>%
-                    MyUtilities::importAggregateExcelSheet(sheet.name)
+                    MyUtilities::importAggregateExcelSheet(sheet)
                 wide_import %>%
                     data.table::setnames(make.unique(names(wide_import))) %>%
-                    tidyr::pivot_longer(cols = -Datum, names_to = "variable")
+                    tidyr::pivot_longer(cols = -Datum, names_to = "variable") %>%
+                    mutate(SubPlot = sheet)
             }, error = function(e) {
                 if (stringr::str_detect(as.character(e), "Values from 'sheet' are not contained in")) {
-                    cat("Skipped file '", basename(.x), "' for sheet '", sheet.name, "' as it was missing.\n", sep = "")
+                    cat("Skipped file '", basename(.x), "' for sheet '", sheet_names, "' as it was missing.\n", sep = "")
                 } else {
                     stop(e)
                 }
             })
         }) %>%
-        data.table::rbindlist(use.names = TRUE, fill = FALSE) %>%
-        arrange(variable, Datum)
+        bind_rows() %>%
+        mutate_at(vars(SubPlot), as.factor) %>%
+        arrange(SubPlot, variable, Datum) %>%
+        select(SubPlot, Datum, variable, value)
     return(full_data)
 })
 
