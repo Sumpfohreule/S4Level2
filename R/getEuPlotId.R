3b14acbc5f40c750cae5fb6ebdc663c3a1ea862a
Lars Isenberg <isenberg.lars@gmail.com>
2020-10-20 19:37:10 +0200

    Fixed createMMFiles and getEuPlotId functions (again)

--- R/getEuPlotId.R
+++ R/getEuPlotId.R
@@ -1,8 +1,10 @@
 ########################################################################################################################
 getEuPlotId <- function(plot.name, sub.plot.name) {
-    plot.id.table <- data.table(
-        plot = rep(c("Altensteig", "Conventwald", "Esslingen", "Heidelberg", "Ochsenhausen"), each = 2), 
+    data.frame(
+        plot = rep(c("Altensteig", "Conventwald", "Esslingen", "Heidelberg", "Ochsenhausen"), each = 2),
         sub.plot = rep(c("Buche", "Fichte"), times = 5),
-        id = c(859, 819, 856, 806, 862, 812, 852, 802, 858, 808))
-    return(plot.id.table[plot == plot.name & sub.plot == sub.plot.name, id])
+        id = c(859, 819, 856, 806, 862, 812, 852, 802, 858, 808)) %>%
+        filter(plot == plot.name) %>%
+        filter(sub.plot == sub.plot.name) %>%
+        pull(id)
 }
