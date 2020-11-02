efcbaaedc90d8315bb9dab5d4bdb5f808da70057
Lars Isenberg <isenberg.lars@gmail.com>
2020-11-02 14:17:25 +0100

    Improved error messages for getEuPlotId and added tests for them

--- tests/testthat/test-getEuPlotId.R
+++ tests/testthat/test-getEuPlotId.R
@@ -1,3 +1,20 @@
 test_that("Functions works in vectorized form", {
     testthat::skip("Integrate tests for getEuPlotId")
 })
+
+test_that("Works with single plot and subplot", {
+    testthat::expect_equal(getEuPlotId("Altensteig", "Fichte"), 819)
+    testthat::expect_equal(getEuPlotId("Ochsenhausen", "Buche"), 858)
+})
+
+test_that("Helpfull error message for missing plot", {
+    testthat::expect_error(getEuPlotId("Entenhausen", "Fichte"), "Für den anggegebene Plot 'Entenhausen' existieren keine Eu Ids")
+})
+
+test_that("Helpfull error message for missing sheet", {
+    testthat::expect_error(getEuPlotId("Altensteig", "Betonwueste"), "Für den anggegebene SubPlot 'Betonwueste' existieren keine Eu Ids")
+})
+
+test_that("Helpfull error message if plot does not have the sheet", {
+    testthat::expect_error(getEuPlotId("Rotenfels", "Buche"), "Plot 'Rotenfels' hat keien Eu Id für den SubPlot 'Buche'")
+})
