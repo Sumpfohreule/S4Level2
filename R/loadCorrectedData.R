26863fdd9fa38352e266ad08c6572657d1959560
Lars Isenberg <isenberg.lars@gmail.com>
2020-11-03 16:40:13 +0100

    Included generic in Plot class as it is only used there

--- R/loadCorrectedData.R
+++
@@ -1,5 +0,0 @@
-########################################################################################################################
-setGeneric("loadCorrectedData", def = function(.Object, sheet_names, years = NULL) {
-        standardGeneric("loadCorrectedData")
-    }
-)
