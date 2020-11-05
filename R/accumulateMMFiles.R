8e5c30e53d345672a88194253b6341d9598d6f06
Lars Isenberg <isenberg.lars@gmail.com>
2020-11-05 20:05:04 +0100

    fixed path in accumulateMMFiles and improved code

--- R/accumulateMMFiles.R
+++ R/accumulateMMFiles.R
@@ -1,52 +1,44 @@
 ########################################################################################################################
 accumulateMMFiles <- function(target.year, output.folder) {
-    mm.year.pattern <- paste0("^[[:alpha:]/]+_", target.year)
-    
-    plm.files <- dir("Data",
-        pattern = paste0(mm.year.pattern, "\\.PLM$"),
-        full.names = TRUE,
-        recursive = TRUE)
-    plm.list <- list()
-    for (plm.file in plm.files) {
-        plm.list[[plm.file]] <- read_delim(plm.file,
+    mm_files <- target.year %>%
+        paste0("^[[:alpha:]/]+_", .) %>%
+        dir("data", pattern = ., full.names = TRUE, recursive = TRUE)
+    assertthat::assert_that(assertthat::not_empty(mm_files))
+
+    column_types <- readr::cols(
+        latitude = "c",
+        longitude = "c",
+        vertical_position = "c",
+        date_monitoring_first = "c",
+        date_monitoring_last = "c")
+    mm_files %>%
+        purrr::keep(~ stringr::str_detect(.x, pattern = "\\.PLM")) %>%
+        purrr::map(~ readr::read_delim(
+            file = .x,
             delim = ";",
-            col_types = cols(latitude = "c", longitude = "c", vertical_position = "c"))
-    }
-    full.plm.table <- rbindlist(plm.list, use.names = TRUE, fill = FALSE)
-    setkey(full.plm.table, plot, instrument_seq_nr)
-    full.plm.table[, `!Sequence` := 1 : .N]
-    plm.file.name <- paste0("04", target.year, ".PLM")
-    write.table(full.plm.table,
-        file = file.path(output.folder,plm.file.name),
-        quote = FALSE,
-        sep = ";",
-        dec = ".",
-        row.names = FALSE,
-        na = "",
-        fileEncoding = "UTF-8")
-    print(paste0("Created ", output.folder, "/", plm.file.name))
-    
-    mem.files <- dir("Data",
-        pattern = paste0(mm.year.pattern, "\\.MEM$"),
-        full.names = TRUE,
-        recursive = TRUE)
-    mem.list <- list()
-    for (mem.file in mem.files) {
-        mem.list[[mem.file]] <- read_delim(mem.file,
+            col_types = column_types)) %>%
+        bind_rows() %>%
+        arrange(plot, instrument_seq_nr) %>%
+        mutate(`!Sequence` = 1:n()) %>%
+        readr::write_delim(
+            path =file.path(output.folder, paste0("04", target.year, ".PLM")),
             delim = ";",
-            col_types = cols())
-    }
-    full.mem.table <- rbindlist(mem.list, use.names = TRUE, fill = FALSE)
-    setkey(full.mem.table, plot, instrument_seq_nr)
-    full.mem.table[, `!Sequence` := 1 : .N]
-    mem.file.name <- paste0("04", target.year, ".MEM")
-    write.table(full.mem.table,
-        file = file.path(output.folder, mem.file.name),
-        quote = FALSE,
-        sep = ";",
-        dec = ".",
-        row.names = FALSE,
-        na = "",
-        fileEncoding = "UTF-8")
-    print(paste0("Created ", output.folder, "/", mem.file.name))
+            na = "",
+            quote_escape = FALSE)
+    print(sprintf("Created PLM file in '%s'", output.folder))
+
+    mm_files %>%
+        purrr::keep(~ stringr::str_detect(.x, pattern = "\\.MEM")) %>%
+        purrr::map(~ readr::read_delim(
+            file = .x,
+            delim = ";",
+            col_types = readr::cols())) %>%
+        bind_rows() %>%
+        arrange(plot, instrument_seq_nr) %>%
+        mutate(`!Sequence` = 1:n()) %>%
+        readr::write_delim(
+            path = file.path(output.folder, paste0("04", target.year, ".MEM")),
+            delim = ";",
+            na = "")
+    print(sprintf("Created MEM file in '%s'", output.folder))
 }
