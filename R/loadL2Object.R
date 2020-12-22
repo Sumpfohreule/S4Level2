562afcf0eb1f11f87cf27edf921d15d7b0464718
Lars Isenberg <isenberg.lars@gmail.com>
2020-12-22 19:02:09 +0100

    changed loadL2Object function so it does not take a path anymore, but uses the new saved path from data/output

--- R/loadL2Object.R
+++ R/loadL2Object.R
@@ -3,7 +3,8 @@
 #' @param data_path A string with the path to the package top level data location
 #' @return An Object of class Level2
 #' @export
-loadL2Object <- function(data_path) {
+loadL2Object <- function() {
+    data_path <- readRDS("data/output/data_location_path.rds")
     level2_file <- dir(data_path, pattern = "Level2.rds$", full.names = TRUE, recursive = TRUE)
     tryCatch(.Object <- readRDS(file = level2_file),
              error = function(e) {
