e18f8ed67cd111998c25bf7083f37c99c6a44470
Lars Isenberg <isenberg.lars@gmail.com>
2020-12-29 17:23:15 +0100

    Moved funcionality from initialization to connect for less code duplication

--- R/initializeDataLocation.R
+++ R/initializeDataLocation.R
@@ -11,25 +11,11 @@ initializeDataLocation <- function(data_path) {
         stop("Can't create data folder in not existing path: ", parent_path)
     }
 
-    structure_path <- file.path(data_path, "internal_structure")
     pre_initialized_locations <- dir(data_path, pattern = "Level2.rds$", recursive = TRUE)
     if (length(pre_initialized_locations) > 0) {
-        stop("S4Level2 location has already been initialized before. 'resetDataLocation()' can be used to reset.")
+        structure_path <- file.path(data_path, "internal_structure")
+        level2 <- Level2(structure_path)
+        saveL2Object(level2)
     }
-
-    dir.create(structure_path, showWarnings = FALSE, recursive = TRUE)
-    level2 <- Level2(structure_path)
-
-    is_MyUtilities_missing <- installed.packages() %>%
-        data.frame() %>%
-        filter(Package == "MyUtilities") %>%
-        ( function(x) { nrow(x) == 0 } )
-    if (is_MyUtilities_missing) {
-        install.packages(
-            system.file("extdata", "MyUtilities_4.1.1.tar.gz", package = "S4Level2"),
-            repos = NULL)
-    }
-    dir.create("data/output", recursive = TRUE, showWarnings = FALSE)
-    saveRDS(data_path, "data/output/data_location_path.rds")
-    saveL2Object(level2)
+    connectToExistingDataLocation(data_path)
 }
