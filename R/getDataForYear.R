1f8745602a48e49335ade7c50adfc0ebd3dc2d5f
Lars Isenberg <isenberg.lars@gmail.com>
2021-01-01 19:45:14 +0100

    Fixed getDataForYear

--- R/getDataForYear.R
+++ R/getDataForYear.R
@@ -4,12 +4,12 @@
 #' @param plot_URIs String (Optional) of a plot name, for which to retrieve data for
 #' @export
 getDataForYear <- function(target_year, plot_URIs) {
-    plotURIs %>%
+    plot_URIs %>%
         purrr::map(~ {
             loadL2Object() %>%
                 getObjectByURI(.x)
         }) %>%
-        getData(.Object, start.date = paste0(target_year - 1, "-12-01"), end.date = paste0(target_year + 1, "-02-01")) %>%
-        filter(lubridate::year(Datum) == target_year) %>%
-        bind_rows()
+        purrr::map(~ getData(.x, start.date = paste0(target_year - 1, "-12-01"), end.date = paste0(target_year + 1, "-02-01"))) %>%
+        bind_rows() %>%
+        filter(lubridate::year(Datum) == target_year)
 }
