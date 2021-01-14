a61be6f33acb894a1844f9dbef654681d4472de4
Lars Isenberg <isenberg.lars@gmail.com>
2021-01-14 14:17:23 +0100

    Created constant df PLOT_IDS for better data storage of these ids

--- R/getEuPlotId.R
+++ R/getEuPlotId.R
@@ -1,16 +1,11 @@
 ########################################################################################################################
 getEuPlotId <- function(plot.name, sub.plot.name) {
-    eu_plot_map <- data.frame(
-        plot = rep(c("Altensteig", "Conventwald", "Esslingen", "Heidelberg", "Ochsenhausen", "Rotenfels"), each = 2),
-        sub.plot = rep(c("Buche", "Fichte"), times = 6),
-        id = c(859, 819, 856, 806, 862, 812, 852, 802, 858, 808, NA, 801))
-
-    if (!(plot.name %in% eu_plot_map[, "plot"])) {
+    if (!(plot.name %in% PLOT_IDS[, "plot"])) {
         stop(sprintf("Für den anggegebene Plot '%s' existieren keine Eu Ids", plot.name))
-    } else if (!(sub.plot.name %in% eu_plot_map[, "sub.plot"])) {
+    } else if (!(sub.plot.name %in% PLOT_IDS[, "sub.plot"])) {
         stop(sprintf("Für den anggegebene SubPlot '%s' existieren keine Eu Ids", sub.plot.name))
     }
-    eu_plot_map %>%
+    PLOT_IDS %>%
         filter(plot == plot.name) %>%
         filter(sub.plot == sub.plot.name) %>%
         pull(id) %>%
