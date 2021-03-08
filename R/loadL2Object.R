49ca6362914503ba12399dd160696d6363e43cf5
Lars Isenberg <isenberg.lars@gmail.com>
2021-03-08 11:22:35 +0100

    Clarified error message for missing DataLocation path and removed comments as it is a private function

--- R/loadL2Object.R
+++ R/loadL2Object.R
@@ -1,13 +1,12 @@
 ########################################################################################################################
-#' Load the top level Level2 Object
-#' @return An Object of class Level2
 loadL2Object <- function() {
     data_path <- readRDS(DATA_PATH_FILE_LOCATION)
     level2_file <- dir(data_path, pattern = "Level2.rds$", full.names = TRUE, recursive = TRUE)
     tryCatch(.Object <- readRDS(file = level2_file),
              error = function(e) {
                  stop("Data directory does not seem to be initialized.\n",
-                      "Change data_path or use initializeDataLocation function")
+                      "Either connect to an existing Level2-Location (S4Level2::connectToExistingDataLocation)\n",
+                      "or create a new one (S4Level2::initializeDataLocation)")
              }
     )
     return(.Object)
