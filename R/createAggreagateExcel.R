04cc27ebdbe246cb9ead4008ad7349b28c5f0217
Isenberg <Lars.Isenberg@forst.bwl.de>
2021-04-26 13:03:12 +0200

    Added wrapper for function from another package

---
+++ R/createAggreagateExcel.R
@@ -0,0 +1,5 @@
+#' @references Level2DataAggregation::createAggregateExcel
+#' @export
+createAggregateExcel <- function(plot_name, year, out_path) {
+    Level2DataAggregation::createAggregateExcel(plot_name = plot_name, year = year, out_path = out_path)
+}
