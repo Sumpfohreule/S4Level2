d8b084eebd18ed02224696201aadccf9acf032ca
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-17 17:15:29 +0200

    removed createAggregateExcel and the dependency on the Level2DataAggregation Package as it led to cyclic dependencies

--- R/createAggreagateExcel.R
+++
@@ -1,5 +0,0 @@
-#' @references Level2DataAggregation::createAggregateExcel
-#' @export
-createAggregateExcel <- function(plot_name, year, out_path) {
-    Level2DataAggregation::createAggregateExcel(plot_name = plot_name, year = year, out_path = out_path)
-}
