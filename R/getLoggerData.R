990b38295851b55e9781238c3dd52f348149e5fc
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-18 14:52:09 +0200

    Removed unused parameters in the generic for getLoggerData

--- R/getLoggerData.R
+++ R/getLoggerData.R
@@ -2,10 +2,6 @@
 setGeneric("getLoggerData", def = function(
         .Object,
         start.date = "1900-01-01",
-        end.date = "2100-01-01",
-        plot = NULL,
-        sub.plot = NULL,
-        logger.name = NULL,
-        as.wide.table = FALSE) {
+        end.date = "2100-01-01") {
         standardGeneric("getLoggerData")
 })
