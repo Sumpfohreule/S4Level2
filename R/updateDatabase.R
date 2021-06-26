669e58a92739f396642afb783705895b9301d104
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2021-06-26 09:50:01 +0200

    Made it possible to select in updateDatabase for one or multple plot subplot combinations

--- R/updateDatabase.R
+++ R/updateDatabase.R
@@ -2,11 +2,20 @@
 #'
 #' This may take a while, especially if run for the first time.
 #' Files are searched for within the setup plot locations.
+#' @param plots Strings either containing plots to update or a wildcard '*' for all plots
+#' @param sub_plots Strings either containing sub plots to update or a wildcard '*' for all sub plots
 #' @export
-updateDatabase <- function() {
+updateDatabase <- function(plots = "*", sub_plots = "*") {
     l2_object <- loadL2Object()
-    for (path in expandURIPlaceholder(l2_object, Level2URI("*/*/*"))) {
-        updated_paths <- getObjectByURI(l2_object, path) %>%
+
+    all_paths <- file.path(plots, sub_plots, "*") %>%
+        purrr::map(Level2URI) %>%
+        purrr::map(~ expandURIPlaceholder(l2_object, .x)) %>%
+        purrr::flatten() %>%
+        purrr::flatten()
+
+    for (object_path in all_paths) {
+        updated_paths <- getObjectByURI(l2_object, object_path) %>%
             updateFilePaths()
         updated_data <- updateData(updated_paths)
         updated_l2 <- replaceObjectByURI(l2_object, updated_data)
